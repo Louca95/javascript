f5852b52ca04d734f8f1df6e37c5fd3bcdc9a358
Louca cascalheira-mercier <cascalheiralouca@gmail.com>
2024-08-01 14:41:51 +0200

    js_tableau

--- js/tableaux.js
+++ js/tableaux.js
@@ -1,122 +1,101 @@
-//Créer un tableau
-//let tableau = new Array(element0, element1, element2);
-//let tableau = Array(element0, element1, element2);
-//let tableau = [element0, element1, element2];
+// TABLEAUX
 
-// déclarer un tableau vide
-let tab = [];
+// Création d'un tableau
 
-let fruits = ["Apple", "Banana"];
-console.log(fruits.length);
-//2
+let array = [];
+console.log(array.length);
 
-let first = fruits[0];
-console.log(first);
-// Apple
+// Les indices des tableaux commencent toujours à la position 0
+let tab = ["Hello", 23];
+console.log(tab);
+console.log(tab.length);
 
-//Boucler sur un tableau
-let couleurs = ["red", "green", "blue"];
-for (let i = 0; i < couleurs.length; i++) {
-	console.log(couleurs[i]);
-	document.write(`<p style='color:${couleurs[i]}'>J'aime les licornes!</p>`);
+// Accéder à une position donnée dans le tableau
+let index = tab[1];
+console.log(index);
+
+// Ajouter un élément à la fin du tableau
+let newElement = array.push("Navigateur");
+console.log(array);
+
+let again = array.push("Internet", "Écran", 13);
+console.log(array);
+
+document.write(`<p style='color:orange'>Sans boucle: ${array}</p>`);
+
+for (let i = 0; i < array.length; ++i) {
+	document.write(`<p style='color:blue'>Après boucle: ${array[i]}</p>`);
 }
 
-// AU LIEU DE MODIFIER LE TABLEAU ON EN CREE UN AUTRE IDENTIQUE
+for (let i = 0; i < array.length; ++i) {
+	if (i == 2) {
+		document.write(`<p style='color:green'>Après boucle: ${array[i]}</p>`);
+	}
+}
 
-//Ajouter à la fin du tableau
-let newArray = fruits.push("Orange");
-// ["Apple", "Banana", "Orange"]
+// Ajouter un élément au début du tableau
+let add = array.unshift("Avion");
+console.log(array);
 
-//Supprimer le dernier élément du tableau
-let last = fruits.pop(); // supprime Orange (à la fin)
-// ["Apple", "Banana"];
+// Supprimer un élément à la fin du tableau
+let supp = array.pop();
+console.log(array);
 
-//Supprimer le premier élément du tableau
-let firstDelete = fruits.shift(); // supprime Apple (au début)
-// ["Banana"];
+// Supprimer un élément au début du tableau
+let start = array.shift();
+console.log(array);
 
-//Ajouter au début du tableau
-let newLength = fruits.unshift("Strawberry"); // ajoute au début
-// ["Strawberry", "Banana"];
+// Supprimer un élément  par son index
+let suppIndex = array.splice(1, 2); // On supprime deux éléments à partir de la 2ème position
+console.log(array);
 
-//Ajouter à la fin du tableau
-//fruits.push("Mango");
-// ["Strawberry", "Banana", "Mango"]
+// LES OBJETS
 
-// Trouver l'index d'un élément dans le tableau
-let pos = fruits.indexOf("Banana");
-// 1
+// Déclarer un objet vide
 
-//Supprimer un élément par son index
-let removedItem = fruits.splice(pos, 1); // supprime 1 élément à la position pos = (1)
-// ["Strawberry", "Mango"]
+let obj = {};
+console.log(obj);
 
-let n = 2;
+// ajouter une nouvelle valeur dans l'objet
+obj.phone = "Iphone";
+console.log(obj);
 
-var vegetables = ["Cabbage", "Turnip", "Radish", "Carrot"];
-
-var removedItems = vegetables.splice(pos, n);
-// n définit le nombre d'éléments à supprimer,
-// à partir de la position pos
-// ["Cabbage", "Carrot"]
-
-// objets
-
-// déclarez une variable voiture qui contiendra un objet;
-// attribuez les identifiants: marque, modèle, couleur et année(sans les accents) et mettez les valeurs que vous voulez;
-
-let voiture = {
-	marque: "Renault",
-	modele: "Mégane",
-	annee: 1994,
-	couleur: "Rouge",
+// Système clé => valeur / key => value
+let obj2 = {
+	// key --- value
+	phone: "Iphone",
+	desktop: "Windows",
 };
 
-console.log(voiture);
-console.log(voiture.modele);
+console.log(obj2);
 
-// Soit la variable ecran qui contient un objet.
-// déclarez la variable result;
-// récupérez la valeur de la propriété resolution;
-// affectez le résultat à result;
-// affichez result avec console.log();
+// pour ajouter un objet dans notre tableau précédent
+let newArray = array.push(obj2);
 
-let ecran = {
-	marque: "Samsung",
-	annee: 2019,
-	resolution: "1920x1080",
-	prix: "400€",
-};
-let result = ecran.resolution;
-console.log(result);
+console.log(array);
 
-// Soit la variable ecran qui contient un object.
-// ajoutez la propriété taille avec une valeur textuelle(ex: 23 pouces);
-// déclarez la variable result et affectez le résultat de la nouvelle propriété à result;
-// affichez result avec console.log();
+let family = [
+	{
+		father: "François",
+		mother: "Jeannine",
+	},
+	{
+		sister: "Lucienne",
 
-let ecran2 = {
-	marque: "Samsung",
-	annee: 2019,
-	resolution: "1920x1080",
-	prix: "400€",
+		phone: "06898798",
+	},
+];
+
+let family2 = {
+	father: "François",
+	mother: "Jeannine",
 };
 
-let result2 = (ecran.taille = "23 pouces");
-console.log(ecran2);
+console.log(family2.father);
 
-// Soit la variable voiture qui contient un objet.
-// utilisez une boucle
-// for in pour parcourir l'objet;
-// dans la boucle affichez chaque valeur avec console.log();
+console.log(family.length);
 
-let voiture2 = {
-	marque: "Bugatti",
-	modele: "Chiron",
-	couleur: "bleue",
-	annee: 2016,
-};
-
-for (let i in voiture2) {
-	console.log(voiture2[i]);
+for (let i = 1; i < family.length; i++) {
+	console.log(family[0].mother);
+	document.write(`<p>${family[1].phone} et ${family[1].sister}</p>`);
 }
